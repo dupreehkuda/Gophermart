077260929d4135f824e530fe423e192f973ef9c2
Danila <84472737+dupreehkuda@users.noreply.github.com>
2022-10-05 18:40:18 +0300

    initial commit \w fix

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"github.com/dupreehkuda/Gophermart/internal/configuration"
+	"github.com/dupreehkuda/Gophermart/internal/handlers"
+	"github.com/dupreehkuda/Gophermart/internal/logger"
+	"github.com/dupreehkuda/Gophermart/internal/middleware"
+	"github.com/dupreehkuda/Gophermart/internal/processors"
+	"github.com/dupreehkuda/Gophermart/internal/server"
+	"github.com/dupreehkuda/Gophermart/internal/storage"
+)
+
+func main() {
+	log := logger.InitializeLogger()
+
+	cfg := configuration.New(log)
+	store := storage.New(cfg.DatabasePath, log)
+	proc := processors.New(store, log)
+	service := handlers.New(store, proc, log)
+	mware := middleware.New(proc, log)
+
+	api := server.New(service, mware, log)
+	api.Launch(cfg.Address)
+}
